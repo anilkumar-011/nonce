611320dbc3788f26ed9c27934009fe8d1a510efe
ANIL KUMAR CHINTHAPALLI <111039342+anilkumar-011@users.noreply.github.com>
2024-11-07 15:43:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, render_template,g
+import os
+import base64
+import secrets
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    nonce = base64.b64encode(secrets.token_bytes(16)).decode('utf-8')
+    g.nonce=nonce
+    return render_template("index.html", nonce=nonce)
+
+
+@app.after_request
+def after_request(response):
+    response.headers["access-control-allow-methods"] = "DELETE, GET, POST, PUT"
+    response.headers["access-control-allow-origin"] = "*"
+    response.headers["access-control-allow-headers"] = "content-type"
+    nonce = getattr(g, 'nonce', None)
+    if nonce:
+        response.headers["Content-Security-Policy"] = f"script-src 'nonce-{nonce}'"
+
+    return response
+
+
+@app.route("/data")
+def data():
+    data = {
+        "name": "flask",
+        "nonce": "yes nonce",
+    }
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
