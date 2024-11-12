5dafbc9a6b812489dc7af952639784fcaac87ed1
ANIL KUMAR CHINTHAPALLI <111039342+anilkumar-011@users.noreply.github.com>
2024-11-12 10:38:51 +0000

    pp

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ def after_request(response):
     response.headers["access-control-allow-methods"] = "DELETE, GET, POST, PUT"
     response.headers["access-control-allow-origin"] = "*"
     response.headers["access-control-allow-headers"] = "content-type"
+    nonce = getattr(g, 'nonce', None)
     css_nonce = getattr(g, 'css_nonce', None)
     if nonce:
         response.headers["Content-Security-Policy"] += f" script-src 'nonce-{nonce}'"
     if css_nonce:
         response.headers["Content-Security-Policy"] = f" style-src 'nonce-{css_nonce}'"
-    nonce = getattr(g, 'nonce', None)
     return response
 
 
