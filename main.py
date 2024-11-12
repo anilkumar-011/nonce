2c1a58f213550e15ccc446d653d3b1c2ed597932
ANIL KUMAR CHINTHAPALLI <111039342+anilkumar-011@users.noreply.github.com>
2024-11-12 10:39:40 +0000

    pp

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ def after_request(response):
     nonce = getattr(g, 'nonce', None)
     css_nonce = getattr(g, 'css_nonce', None)
     if nonce:
-        response.headers["Content-Security-Policy"] += f" script-src 'nonce-{nonce}'"
+        response.headers["Content-Security-Policy"] = f" script-src 'nonce-{nonce}'"
     if css_nonce:
-        response.headers["Content-Security-Policy"] = f" style-src 'nonce-{css_nonce}'"
+        response.headers["Content-Security-Policy"] += f" style-src 'nonce-{css_nonce}'"
     return response
 
 
