2d128c603647e0e9d193c1509aaac2a1378e77b8
ANIL KUMAR CHINTHAPALLI <111039342+anilkumar-011@users.noreply.github.com>
2024-11-14 06:14:41 +0000

    ww

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ def after_request(response):
     response.headers["access-control-allow-headers"] = "content-type"
     nonce = getattr(g, 'nonce', None)
     css_nonce = getattr(g, 'css_nonce', None)
-    if nonce:
-        response.headers["Content-Security-Policy"] = f" script-src 'nonce-{nonce}'"
     if css_nonce:
-        response.headers["Content-Security-Policy"] += f" style-src 'nonce-{css_nonce}'"
+        response.headers["Content-Security-Policy"] = f" style-src 'nonce-{css_nonce}'"
+    if nonce:
+        response.headers["Content-Security-Policy"] += f" script-src 'nonce-{nonce}'"
     return response
 
 
